2442b9d7db7342a133350d0b532fb364f7c80414
Mateusz Ostafil <mateusz.ostafil@codecool.com>
2017-05-07 00:53:58 +0200

    Add examination for children

--- App/Lib/ExaminationTemplate.js
+++ App/Lib/ExaminationTemplate.js
@@ -1,5 +1,6 @@
 import template1 from "../data/templates/rachunek-dla-doroslych.json";
+import template2 from "../data/templates/rachunek-dla-dzieci.json"
 
 export function list() {
-  return Promise.resolve([template1]);
+  return Promise.resolve([template1, template2]);
 }
