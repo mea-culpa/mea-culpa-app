0559ae07c3360f542c91cb7dcd4777c2d012d52f
Mateusz Ostafil <mateusz.ostafil@codecool.com>
2017-05-06 22:56:06 +0200

    Fix delete button hiding in summary component

--- App/Components/ExaminationSummary.js
+++ App/Components/ExaminationSummary.js
@@ -12,6 +12,22 @@ import {
 
 import {deleteFile} from '../Lib/Examination';
 
+
+const Footer = (props) => (
+  <View>
+    <Button
+      onPress={() => {
+        deleteFile(props.examination.filename)
+        props.onExaminationDelete()
+      }}
+      title="Usuń"
+      color="#841584"
+      accessibilityLabel="Usuń"
+      style={{margin: 20, width: 50}}
+    />
+  </View>
+);
+
 export default class ExaminationSummary extends Component {
   constructor(props) {
     super(props);
@@ -25,24 +41,29 @@ export default class ExaminationSummary extends Component {
 
   render() {
     return (
-
       <View style={{
         flex: 1,
         flexDirection: 'column',
-        justifyContent: 'space-around',
+        justifyContent: 'center',
       }}>
-        <Text>{this.props.examination.name}</Text>
-        <Text>Twoje grzechy:</Text>
-        <Button
-          onPress={() => {
-            deleteFile(this.props.examination.filename)
-            this.props.onExaminationDelete()
-          }}
-          title="Usuń"
-          color="#841584"
-          accessibilityLabel="Usuń"
+        <Text style={styles.title}>{this.props.examination.name}</Text>
+        <Text style={{marginLeft: 10,}}>Twoje grzechy:</Text>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={(rowData) => <Text style={{marginLeft: 10,}}>{`\u2022 ${rowData.name}`}</Text>}
+          renderFooter={() =>
+            <Footer examination={this.props.examination} onExaminationDelete={this.props.onExaminationDelete}/>
+          }
         />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 25,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
