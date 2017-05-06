6c5259cbbfea0a6df10b1bddd46b2fa660385144
Michal Raczka <michal@raczka.me>
2017-05-06 22:54:35 +0200

    Fix the summary update

--- App/Components/ExaminationSummary.js
+++ App/Components/ExaminationSummary.js
@@ -15,17 +15,12 @@ import {deleteFile} from '../Lib/Examination';
 export default class ExaminationSummary extends Component {
   constructor(props) {
     super(props);
-    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-    this.state = {
-      //TODO: this does not render the updated values
-      dataSource: ds.cloneWithRows(props.examination.questions.filter((question) => question.sin)),
-    };
-
   }
 
   render() {
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    const dataSource = ds.cloneWithRows(this.props.examination.questions.filter((question) => question.sin));
     return (
-
       <View style={{
         flex: 1,
         flexDirection: 'column',
@@ -33,6 +28,10 @@ export default class ExaminationSummary extends Component {
       }}>
         <Text>{this.props.examination.name}</Text>
         <Text>Twoje grzechy:</Text>
+        <ListView
+          dataSource={dataSource}
+          renderRow={(rowData) => <Text>*{rowData.name}</Text>}
+        />
         <Button
           onPress={() => {
             deleteFile(this.props.examination.filename)
