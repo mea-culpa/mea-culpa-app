ea3380e76b651ecbbf80512ceebc377f5bbee014
Michal Raczka <michal@raczka.me>
2017-05-07 01:17:02 +0200

    Simplify question component

--- App/Components/ExaminationQuestion.js
+++ App/Components/ExaminationQuestion.js
@@ -7,7 +7,7 @@ import {
   Image,
   TextInput,
   StyleSheet,
-  TouchableHighlight
+  TouchableWithoutFeedback
 } from 'react-native';
 
 
@@ -15,31 +15,21 @@ import {
 export default class ExaminationQuestion extends Component {
   constructor(props) {
     super(props);
-    this.state = {question: props.question};
     this.updateNotes = this.updateNotes.bind(this);
     this.setSin = this.setSin.bind(this);
   }
 
   updateNotes(text) {
-    this.setState((state) => {
-      let newQuestion = state.question;
-      newQuestion.notes = text
-      this.props.onQuestionUpdate(this.key, newQuestion);
-      return {
-        question: newQuestion,
-      };
-    });
+    let newQuestion = this.props.question;
+    newQuestion.notes = text
+    this.props.onQuestionUpdate(this.key, newQuestion);
   };
 
   setSin(sin) {
-    this.setState((state) => {
-      let newQuestion = state.question;
-      newQuestion.sin = sin;
-      this.props.onQuestionUpdate(this.key, newQuestion);
-      return{
-        question: newQuestion,
-      };
-    });
+    let newQuestion = this.props.question;
+    newQuestion.sin = sin;
+    console.log("setSin", this.props.question, newQuestion, this.key);
+    this.props.onQuestionUpdate(this.key, newQuestion);
   };
 
   render() {
@@ -49,29 +39,29 @@ export default class ExaminationQuestion extends Component {
           flexDirection: 'column',
           justifyContent: 'space-around',
         }}>
-          <Text style={styles.category}>{this.state.question.category}</Text>
-          <Text style={styles.description}>{this.state.question.description}</Text>
-          <Text style={styles.question}>{this.state.question.name}</Text>
+          <Text style={styles.category}>{this.props.question.category}</Text>
+          <Text style={styles.description}>{this.props.question.description}</Text>
+          <Text style={styles.question}>{this.props.question.name}</Text>
 
           <View style={{
             flex: 0,
             flexDirection: 'row',
             justifyContent: 'space-around',
           }}>
-            <TouchableHighlight underlayColor="white" onPress={() => this.setSin(true)}>
+            <TouchableWithoutFeedback underlayColor="white" onPress={() => this.setSin(true)}>
               <Image source={require('./../Img/devil.png')}
-                style={this.state.question.sin ? [styles.icon, styles.iconActive] : styles.icon} />
-            </TouchableHighlight>
-            <TouchableHighlight underlayColor="white" onPress={() => this.setSin(false)}>
+                style={this.props.question.sin ? [styles.icon, styles.iconActive] : styles.icon} />
+            </TouchableWithoutFeedback>
+            <TouchableWithoutFeedback underlayColor="white" onPress={() => this.setSin(false)}>
               <Image source={require('./../Img/angel.png')}
-                style={this.state.question.sin ? styles.icon : [styles.icon, styles.iconActive]} />
-            </TouchableHighlight>
+                style={this.props.question.sin ? styles.icon : [styles.icon, styles.iconActive]} />
+            </TouchableWithoutFeedback>
           </View>
 
           <KeyboardAvoidingView
             behavior='padding'>
             <TextInput
-              defaultValue={this.state.question.notes}
+              defaultValue={this.props.question.notes}
               onChangeText={(text) => this.updateNotes(text)}
               multiline={true}
               placeholder="Miejsce na twoje notatki..."
