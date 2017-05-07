97249623b13f5ab8c496a2147e57ef0c72fe9d3f
Mateusz Ostafil <mateusz.ostafil@codecool.com>
2017-05-07 13:27:21 +0200

    Add ten commandments to examination templates

--- App/Lib/ExaminationTemplate.js
+++ App/Lib/ExaminationTemplate.js
@@ -1,8 +1,8 @@
 import template1 from "../data/templates/rachunek-dla-doroslych.json";
 import template2 from "../data/templates/rachunek-dla-dzieci.json"
 import template3 from "../data/templates/rachunek-dla-doroslych-2.json"
-
+import template4 from "../data/templates/dekalog.json"
 
 export function list() {
-  return Promise.resolve([template1, template2, template3]);
+  return Promise.resolve([template1, template2, template3, template4]);
 }
